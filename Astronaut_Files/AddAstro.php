ac8be4e3c0dd9f4fcae9cee6945016ed9a56a69b
Thomas Welch <91126396+Twelch115@users.noreply.github.com>
2021-11-29 12:40:29 +0000

    Update AddAstro.php

--- Astronaut_Files/AddAstro.php
+++ Astronaut_Files/AddAstro.php
@@ -3,6 +3,5 @@
 require '../Connection.php'; #the file will not run without Connection.PHP being linked
 require '../Validation.php'; #the file will not run without Validation.PHP being linked
 
-Validate("testing 1...2...3");
-#echo 
+echo Validate("testing 1...2...3");
 #ends PHP code block ?>
