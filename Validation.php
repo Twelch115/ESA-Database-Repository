0ab24d09edd11b6871fdbb7c78a87810c9f8c29e
Thomas Welch <91126396+Twelch115@users.noreply.github.com>
2021-11-29 12:36:50 +0000

    Update and rename Examples/Validation.php to Validation.php

---
+++ Validation.php
@@ -0,0 +1,8 @@
+<?php #starts PHP code block
+function Validate($Data) { #sets function name and paramiters
+	$Data = trim($Data); #removes blank spaces from the given data
+	$Data = stripslashes($Data); #removes \ from the given data
+	$Data = htmlspecialchars($Data); #sets special characters to HTML compatable versions.
+	return $Data; #returns paramater
+}
+#ends PHP code block ?>
