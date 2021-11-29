e20b9046bef1b33fa9a2556563144cf534a79ac7
Thomas Welch <91126396+Twelch115@users.noreply.github.com>
2021-11-29 12:02:25 +0000

    Update Validation.php

--- Examples/Validation.php
+++ Examples/Validation.php
@@ -1,11 +1,15 @@
 <?php 
-function Validate(Data) {
-	Data = trim(Data);
-	Data = stripslashes(Data);
-	Data = htmlspecialchars(Data);
-	return Data 
+function Validate($Data) {
+	$Data = trim($Data);
+	$Data = stripslashes($Data);
+	$Data = htmlspecialchars($Data);
+	return $Data;
 }
 
-Text = " \a the $*$% best \n"
-echo "Valdiate(Text)";
+
+$myData = "This is some <b>bold</b> text.";
+echo htmlspecialchars($myData);
+
+Validate($myData);
+echo $myData;
 ?>
