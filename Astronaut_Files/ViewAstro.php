f264bbaa5fdc0ecfd74c6610a8ac03d2f42238fb
Thomas Welch <91126396+Twelch115@users.noreply.github.com>
2021-11-30 11:27:35 +0000

    Update ViewAstro.php

--- Astronaut_Files/ViewAstro.php
+++ Astronaut_Files/ViewAstro.php
@@ -3,7 +3,18 @@
 require '../Connection.php'; #the file will not run without Connection.PHP being linked
 require '../Validation.php'; #the file will not run without Validation.PHP being linked
 
-$Data = $sql = "SELECT * FROM `astronaut`"; #SQL statement to present all data in the astronaut table
-echo $Data;
+$sql = "SELECT * FROM `astronaut`"; #SQL statement to present all data in the astronaut table
+
+<table>
+  <tr>
+    <th> ID </th>
+    <th> Name </th>
+    <th> No. Missions </th>
+  </tr>
+  <tr>
+    <td> </td>
+  </tr>
+</table>
+
 #ends PHP code block
 ?>
