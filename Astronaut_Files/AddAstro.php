572d87eed86bc471423d9d81b1d695983c920b73
Thomas Welch <91126396+Twelch115@users.noreply.github.com>
2021-11-30 09:32:40 +0000

    Update AddAstro.php

--- Astronaut_Files/AddAstro.php
+++ Astronaut_Files/AddAstro.php
@@ -3,5 +3,21 @@
 require '../Connection.php'; #the file will not run without Connection.PHP being linked
 require '../Validation.php'; #the file will not run without Validation.PHP being linked
 
-echo Validate("\ \ / < bold > % $ testing ");
+
 #ends PHP code block ?>
+
+$AstroName = $_POST["AstroName"]
+$AstroMissions =  $_POST["AstroMissions"]
+
+$sql = "INSTERT INTO astronaut(name, no_missions) VALUES ('$AstroName', $AstroMissions)";
+
+if(!mysqli_query($connection, $sql)){
+	die("Error:".mysqli_error($connection));
+}
+else{
+	echo "Data Inputed";
+}
+
+mysqli_close($connection);
+
+?>
