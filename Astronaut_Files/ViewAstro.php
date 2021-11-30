c46f8dad9d12ebade8ccd89ae2b45fb2645db476
Thomas Welch <91126396+Twelch115@users.noreply.github.com>
2021-11-30 10:19:00 +0000

    Create ViewAstro.php

---
+++ Astronaut_Files/ViewAstro.php
@@ -0,0 +1,9 @@
+<?php #starts PHP code block
+
+require '../Connection.php'; #the file will not run without Connection.PHP being linked
+require '../Validation.php'; #the file will not run without Validation.PHP being linked
+
+$sql = "SELECT * FROM `astronaut`"; #SQL statement to present all data in the astronaut table
+
+#ends PHP code block
+?>
