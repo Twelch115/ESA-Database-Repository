e04723c4c93c54b1a68ec9338613e7c9c7c05729
Thomas Welch <91126396+Twelch115@users.noreply.github.com>
2021-11-30 09:23:30 +0000

    Update Validation.php

--- Validation.php
+++ Validation.php
@@ -1,7 +1,10 @@
 <?php #starts PHP code block
 function Validate($Data) { #sets function name and paramiters
+	echo $Data;
 	$Data = trim($Data); #removes blank spaces from the given data
+	echo $Data;
 	$Data = stripslashes($Data); #removes \ from the given data
+	echo $Data;
 	$Data = htmlspecialchars($Data); #sets special characters to HTML compatable versions.
 	return $Data; #returns paramater
 }
