3a3b3b78619d49063d91bfb8ede8c14b36f5b439
Thomas Welch <91126396+Twelch115@users.noreply.github.com>
2021-12-07 09:45:56 +0000

    Update AddAstronaut.php

--- Astronaut_Files/AddAstronaut.php
+++ Astronaut_Files/AddAstronaut.php
@@ -1,25 +1,26 @@
 <?php #starts PHP code block
-require '../Connection.php'; #the file will not run without Connection.PHP being linked
-require '../Validation.php'; #the file will not run without Validation.PHP being linked
+echo "hello";
+// require '../Connection.php'; #the file will not run without Connection.PHP being linked
+// require '../Validation.php'; #the file will not run without Validation.PHP being linked
 
 
-$AstroName = Validate($_POST["AstroName"]);
-$AstroMissions = $_POST["AstroMissions"];
+// $AstroName = Validate($_POST["AstroName"]);
+// $AstroMissions = $_POST["AstroMissions"];
 
-$sql = "INSERT INTO astronaut(name, no_missions) VALUES ('$AstroName', $AstroMissions)";
+// $sql = "INSERT INTO astronaut(name, no_missions) VALUES ('$AstroName', $AstroMissions)";
 
-if(!mysqli_query($connection, $sql)){
-	die("Error:".mysqli_error($connection));
-}
-else{
-	echo "Data Inputed,";
-}
+// if(!mysqli_query($connection, $sql)){
+// 	die("Error:".mysqli_error($connection));
+// }
+// else{
+// 	echo "Data Inputed,";
+// }
 
-mysqli_close($connection);
+// mysqli_close($connection);
 
-echo '<a href = "../index.html"> Return  </a>' #return to index
-echo '<a href = "AddAstro.html"> Add another entry  </a>' #return to add
-echo '<a href = "RemoveAstro.html"> Remove an entry </a>' #return to remove
+// echo '<a href = "../index.html"> Return  </a>' #return to index
+// echo '<a href = "AddAstro.html"> Add another entry  </a>' #return to add
+// echo '<a href = "RemoveAstro.html"> Remove an entry </a>' #return to remove
 
-#ends PHP code block 
+// #ends PHP code block 
 ?>
