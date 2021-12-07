6593831f1032baac2a9bed50bda29c6e0b34c12b
Thomas Welch <91126396+Twelch115@users.noreply.github.com>
2021-12-07 12:05:24 +0000

    Update SpecialAddAttendance.php

--- Attends_Files/SpecialAddAttendance.php
+++ Attends_Files/SpecialAddAttendance.php
@@ -1,16 +1,3 @@
-<?php
-require '../Connection.php'; #the file will not run without Validation.PHP being linked
-require '../Validation.php'; #the file will not run without Validation.PHP being linked
-$sql = "SELECT * FROM targets"; #select all columns from astronaut table
-$result = mysqli_query($connection, $sql); #place connection and sql into a query in result variable
-mysqli_close($connection);
-foreach ($result as $row ) {
-	$thisValue = $row['target_id'];
-	echo $thisValue;
-//                 echo "<option  value= $thisValue >" . $thisValue . "</option>";
-}  
-?>
-
 <!DOCTYPE html> <!-- sets the type of code to be used in the document -->
 <html> <!-- begins the HTML code block -->
 
@@ -21,6 +8,17 @@ foreach ($result as $row ) {
 <div class="mb-3">
     <label for="type" class="form-label">Target ID:</label>
 	<select id="target_id" name="target_id">
+		<?php
+		require '../Connection.php'; #the file will not run without Validation.PHP being linked
+		require '../Validation.php'; #the file will not run without Validation.PHP being linked
+		$sql = "SELECT * FROM targets"; #select all columns from astronaut table
+		$result = mysqli_query($connection, $sql); #place connection and sql into a query in result variable
+		mysqli_close($connection);
+		foreach ($result as $row ) {
+			$thisValue = $row['target_id'];
+			echo "<option  value= $thisValue >" . $thisValue . "</option>";
+		}  
+		?>
         </select> 
 </div>
 </body>
