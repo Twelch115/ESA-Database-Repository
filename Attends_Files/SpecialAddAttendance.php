31f2960523b1da1da9084cc6ff5e4eab235e42e3
Thomas Welch <91126396+Twelch115@users.noreply.github.com>
2021-12-07 11:54:10 +0000

    Update SpecialAddAttendance.php

--- Attends_Files/SpecialAddAttendance.php
+++ Attends_Files/SpecialAddAttendance.php
@@ -1,3 +1,16 @@
 <?php
 echo " I am here";
 ?>
+
+<!DOCTYPE html> <!-- sets the type of code to be used in the document -->
+<html> <!-- begins the HTML code block -->
+
+<head> <!-- opens header tag -->
+	<title> Add an Attendance </title> <!-- Sets Page title -->
+</head> <!-- closes header tag -->
+<body>
+<div class="mb-3">
+    <label for="type" class="form-label">Target ID:</label> 
+</div>
+</body>
+</html>
