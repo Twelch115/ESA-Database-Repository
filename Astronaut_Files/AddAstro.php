7799efdafb4dc717f8043e299088474df351ab5f
Thomas Welch <91126396+Twelch115@users.noreply.github.com>
2021-12-07 10:14:13 +0000

    Update AddAstro.php

--- Astronaut_Files/AddAstro.php
+++ Astronaut_Files/AddAstro.php
@@ -1,5 +1,4 @@
 <?php #starts PHP code block
-
 require '../Connection.php'; #the file will not run without Connection.PHP being linked
 require '../Validation.php'; #the file will not run without Validation.PHP being linked
 
