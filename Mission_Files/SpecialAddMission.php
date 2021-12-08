d11ffb4cc9f2dc07393e01fb8c89dd148adea68b
Thomas Welch <91126396+Twelch115@users.noreply.github.com>
2021-12-08 15:18:02 +0000

    Update SpecialAddMission.php

--- Mission_Files/SpecialAddMission.php
+++ Mission_Files/SpecialAddMission.php
@@ -8,10 +8,14 @@
 <p> Input a new mission into the database </p> 
 <a href = ../index.html> Cancel Addition </a> <br> <br> <!-- links back to the index page -->
 <form action = "AddMission.php" method = "post"> <!-- starts the form, sets the action on submission and the method used -->
-<input type = "text" style = "width:250px;" name = "MissName" placeholder = "Input the missions name." minlength = "1" maxlength = "30" required> <br> <br> <!-- sets the name input box, with it's name, style and length -->
-<input type = "number" style = "width:300px;" name = "MissDeparture" placeholder = "Input the missions departure date. (ddmmyyyy)" min = "00000001" max = "99999999"> <br> <br> <!-- sets the departure date input box, with it's name and min/max values -->
-<input type = "text" style = "width:250px;" name = "MissType" placeholder = "Input the missions type." minlength = "1" maxlength = "30" required> <br> <br> <!-- sets the mission type input box, with it's name, style and length -->
-<input type = "number" style = "width:250px" name = "MissCrew" placeholder = "Input the size of the missions crew." min = "1"  required> <br> <br> <!-- sets the crew size input box, with it's name, style and min value -->	
+<label for="type" class="form-label">Mission Name:</label> <!-- sets the description label -->	
+<input type = "text" style = "width:250px;" name = "MissName"  minlength = "1" maxlength = "30" required> <br> <br> <!-- sets the name input box, with it's name, style and length -->
+<label for="type" class="form-label">Mission Departure Date (DDMMYYYY):</label> <!-- sets the description label -->	
+<input type = "number" style = "width:250px;" name = "MissDeparture" min = "00000001" max = "99999999"> <br> <br> <!-- sets the departure date input box, with it's name and min/max values -->
+<label for="type" class="form-label">Mission type:</label> <!-- sets the description label -->	
+<input type = "text" style = "width:250px;" name = "MissType" minlength = "1" maxlength = "30" required> <br> <br> <!-- sets the mission type input box, with it's name, style and length -->
+<label for="type" class="form-label">Mission Crew size:</label> <!-- sets the description label -->		
+<input type = "number" style = "width:250px" name = "MissCrew" min = "1" required> <br> <br> <!-- sets the crew size input box, with it's name, style and min value -->	
 <div class="mb-3">
     	<label for="type" class="form-label">Target ID:</label>
 	<select id="target_id" name="target_id">
