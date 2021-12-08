c467c95448875918f474e432003944f8c5e8122b
Thomas Welch <91126396+Twelch115@users.noreply.github.com>
2021-12-08 15:56:35 +0000

    Delete pain.php

--- pain.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'Connection.php'; #the file will not run without Connection.PHP being linked
-require 'Validation.php'; #the file will not run without Validation.PHP being linked
-echo "suffering";
-?>
