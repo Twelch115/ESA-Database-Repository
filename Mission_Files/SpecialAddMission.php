48a350cb69e95af5e2286bfe51a3e8e2520ffed7
Thomas Welch <91126396+Twelch115@users.noreply.github.com>
2021-12-08 15:20:07 +0000

    Update SpecialAddMission.php

--- Mission_Files/SpecialAddMission.php
+++ Mission_Files/SpecialAddMission.php
@@ -31,6 +31,22 @@
 		}  
 		?>
         </select> 
+</div>	
+<div class="mb-3">
+    	<label for="type" class="form-label">Target ID:</label>
+	<select id="target_id" name="target_id">
+		<?php
+		require '../Connection.php'; #the file will not run without Validation.PHP being linked
+		require '../Validation.php'; #the file will not run without Validation.PHP being linked
+		$sql = "SELECT * FROM targets"; #select all columns from astronaut table
+		$result = mysqli_query($connection, $sql); #place connection and sql into a query in result variable
+		mysqli_close($connection);
+		foreach ($result as $row ) {
+		$thisValue = $row['target_id'];
+		echo "<option  value= $thisValue >" . $thisValue . "</option>";
+		}  
+		?>
+        </select> 
 </div>
 </body>
 </html>
