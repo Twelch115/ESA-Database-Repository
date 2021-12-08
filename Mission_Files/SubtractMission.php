1597759be18650883a0d01710a71a7d6a26a4ddf
Thomas Welch <91126396+Twelch115@users.noreply.github.com>
2021-12-08 16:52:58 +0000

    Create SubtractMission.php

---
+++ Mission_Files/SubtractMission.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html> <!-- sets the type of code to be used in the document -->
+<html> <!-- begins the HTML code block -->
+<head> <!-- opens header tag -->
+<title> Remove a mission </title> <!-- Sets Page title -->
+<link rel="icon" href="../Images/Rockets.jpg"> <!-- creates icon -->
+</head> <!-- closes header tag -->
+ 
+<body> <!-- opens body tag -->
+<p> Remove a mission from the database </p> 
+<a href = ../index.html> Cancel Removal <a> <br> <br> <!-- links back to the index page -->
+<form action = "RemoveMission.php" method = "post"> <!-- starts the form, sets the action on submission and the method used -->
+<div class="mb-3"> <!-- opens DIV -->
+    <label for="type" class="form-label">Name of mission to be removed:</label> <br> <!-- sets the description label -->
+    <select name="MissName"> <!-- sets name of selection box -->
+	<?php
+	require '../Connection.php'; #the file will not run without Validation.PHP being linked
+	require '../Validation.php'; #the file will not run without Validation.PHP being linked    
+	$sql = "SELECT * FROM Mission"; #select all columns from astronaut table
+	$result = mysqli_query($connection, $sql); #place connection and sql into a query in result variable
+	mysqli_close($connection); #ends SQL database connection
+	foreach ($result as $row ) { #for each row in $result
+	$thisValue = $row['name']; #set $ThisValue as current row
+	echo "<option value= $thisValue >" . $thisValue . "</option>"; #print row as option
+	}  
+	?>
+    </select> <!-- closes selection -->
+</div> <br> <!-- closes DIV -->  
+<input type = "submit" name = "MissSubmit" value = "Submit removal"> <!-- sets the form submission box -->
+</form>	 <!-- ends the form -->	
+</body>
+</html>
