1ade19f780e410cc8b75db9f918c7f38596d5117
Thomas Welch <91126396+Twelch115@users.noreply.github.com>
2021-12-08 15:54:45 +0000

    Update AddAstro.php

--- Astronaut_Files/AddAstro.php
+++ Astronaut_Files/AddAstro.php
@@ -1,3 +1,9 @@
+<html> <!-- begins HTML code block --> 
+<head> <!-- opens header -->
+<link rel="icon" href="../Images/Rockets.jpg"> <!-- creates icon -->	
+</head>	<!-- closes header -->
+</html>	 <!-- ends HTML code block -->
+
 <?php #starts PHP code block
 require '../Connection.php'; #the file will not run without Connection.PHP being linked
 require '../Validation.php'; #the file will not run without Validation.PHP being linked
