a6e4c41ce04d2c194c3de5e70e517909dbe02057
Thomas Welch <91126396+Twelch115@users.noreply.github.com>
2021-12-08 15:59:14 +0000

    Update SubtractAstro.php

--- Astronaut_Files/SubtractAstro.php
+++ Astronaut_Files/SubtractAstro.php
@@ -6,11 +6,11 @@
 	<link rel="icon" href="../Images/Rockets.jpg"> <!-- creates icon -->
 </head> <!-- closes header tag -->
 <body> <!-- opens body tag -->
-	<p> Remove an astronaut from the database </p> 
-	<a href = ../index.html> Cancel Removal <a> <br> <br> <!-- links back to the index page -->
-	<form action = "RemoveAstro.php" method = "post"> <!-- starts the form, sets the action on submission and the method used -->
+<p> Remove an astronaut from the database </p> 
+<a href = ../index.html> Cancel Removal <a> <br> <br> <!-- links back to the index page -->
+<form action = "RemoveAstro.php" method = "post"> <!-- starts the form, sets the action on submission and the method used -->
   
-  <input type = "submit" name = "MissSubmit" value = "Submit addition"> <!-- sets the form submission box -->
-  </form>	 <!-- ends the form -->	
+<input type = "submit" name = "MissSubmit" value = "Submit addition"> <!-- sets the form submission box -->
+</form>	 <!-- ends the form -->	
 </body>
 </html>
