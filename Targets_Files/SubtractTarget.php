06e0c0f1dbf11ec2fb7fae0161e33f81b9b4217f
Thomas Welch <91126396+Twelch115@users.noreply.github.com>
2021-12-09 10:02:57 +0000

    Update SubtractTarget.php

--- Targets_Files/SubtractTarget.php
+++ Targets_Files/SubtractTarget.php
@@ -10,12 +10,12 @@
 <a href = ../index.html> Cancel Removal <a> <br> <br> <!-- links back to the index page -->
 <form action = "RemoveTarget.php" method = "post"> <!-- starts the form, sets the action on submission and the method used -->
 <div class="mb-3"> <!-- opens DIV -->
-    <label for="type" class="form-label">ID of astronaut to be removed:</label> <br> <!-- sets the description label -->
+    <label for="type" class="form-label">Name of target to be removed:</label> <br> <!-- sets the description label -->
     <select name="TarName"> <!-- sets name of selection box -->
 	<?php
 	require '../Connection.php'; #the file will not run without Validation.PHP being linked
 	require '../Validation.php'; #the file will not run without Validation.PHP being linked    
-	$sql = "SELECT * FROM target"; #select all columns from astronaut table
+	$sql = "SELECT * FROM targets"; #select all columns from astronaut table
 	$result = mysqli_query($connection, $sql); #place connection and sql into a query in result variable
 	mysqli_close($connection); #ends SQL database connection
 	foreach ($result as $row ) { #for each row in $result
