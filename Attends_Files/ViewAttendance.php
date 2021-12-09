1b8a8226477a46173efb79027d4b5b0691d9886b
Thomas Welch <91126396+Twelch115@users.noreply.github.com>
2021-12-09 10:29:38 +0000

    Update ViewAttendance.php

--- Attends_Files/ViewAttendance.php
+++ Attends_Files/ViewAttendance.php
@@ -1,3 +1,10 @@
+<html> <!-- begins HTML code block --> 
+<head> <!-- opens header -->
+<title> Add an astronaut </title> <!-- Sets Page title -->	
+<link rel="icon" href="../Images/Rockets.jpg"> <!-- creates icon -->	
+</head>	<!-- closes header -->
+</html>	 <!-- ends HTML code block -->
+
 <?php #starts PHP code block
 require '../Connection.php'; #the file will not run without Connection.PHP being linked
 require '../Validation.php'; #the file will not run without Validation.PHP being linked
