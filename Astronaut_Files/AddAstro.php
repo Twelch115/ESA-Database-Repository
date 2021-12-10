764e3e64fe00b194078285649a5d8defcd2c6c4f
Thomas Welch <91126396+Twelch115@users.noreply.github.com>
2021-12-10 09:13:02 +0000

    Update AddAstro.php

--- Astronaut_Files/AddAstro.php
+++ Astronaut_Files/AddAstro.php
@@ -9,19 +9,19 @@
 require '../Connection.php'; #the file will not run without Connection.PHP being linked
 require '../Validation.php'; #the file will not run without Validation.PHP being linked
 
-$AstroName = Validate($_POST["AstroName"]);
-$AstroMissions = $_POST["AstroMissions"];
+$AstroName = Validate($_POST["AstroName"]); #set data from form as variable
+$AstroMissions = $_POST["AstroMissions"]; #set data from form as variable
 
-$sql = "INSERT INTO astronaut(name, no_missions) VALUES ('$AstroName', $AstroMissions)";
+$sql = "INSERT INTO astronaut(name, no_missions) VALUES ('$AstroName', $AstroMissions)"; # create query to add data to table
 
-if(!mysqli_query($connection, $sql)){
-	die("Error:".mysqli_error($connection));
+if(!mysqli_query($connection, $sql)){ #IF the connection fails
+	die("Error:".mysqli_error($connection)); #end code and print error message
 }
-else{
-	echo "Data Inputed, ";
+else{ #ELSE
+	echo "Data Inputed, "; #print success message
 }
 
-mysqli_close($connection);
+mysqli_close($connection); #ends connection with SQL database
 
 echo '<a href = "../index.html">Return </a>' #return to index
 
