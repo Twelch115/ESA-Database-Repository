1a788870f1315a9c65919c3b8c0730b45f505b58
Thomas Welch <91126396+Twelch115@users.noreply.github.com>
2021-12-10 09:24:45 +0000

    Update AddTarget.php

--- Targets_Files/AddTarget.php
+++ Targets_Files/AddTarget.php
@@ -2,21 +2,21 @@
 require '../Connection.php'; #the file will not run without Connection.PHP being linked
 require '../Validation.php'; #the file will not run without Validation.PHP being linked
 
-$TarName = Validate($_POST["TarName"]);
-$TarFirstDate = $_POST["TarFirst"];
-$TarType = Validate($_POST["TarType"]);
-$TarMissions = $_POST["TarMissions"];
+$TarName = Validate($_POST["TarName"]); #set data from form as variable
+$TarFirstDate = $_POST["TarFirst"]; #set data from form as variable
+$TarType = Validate($_POST["TarType"]); #set data from form as variable
+$TarMissions = $_POST["TarMissions"]; #set data from form as variable
 
-$sql = "INSERT INTO targets(name, first_mission, type, no_missions) VALUES ('$TarName', '$TarFirst', '$TarType', $TarMissions)";
-
-if(!mysqli_query($connection, $sql)){
-	die("Error:".mysqli_error($connection));
+$sql = "INSERT INTO targets(name, first_mission, type, no_missions) VALUES ('$TarName', '$TarFirst', '$TarType', $TarMissions)"; # create query to remove data from table at given id
+ 
+if(!mysqli_query($connection, $sql)){ #IF the connection fails
+	die("Error:".mysqli_error($connection)); #end code and print error message
 }
-else{
-	echo "Data Inputed, " ;
+else{ #ELSE
+	echo "Data Inputed, " ; #print success message
 }
 
-mysqli_close($connection);
+mysqli_close($connection); #ends connection with SQL database
 
 echo '<a href = "../index.html">Return </a>' #return to index
 
