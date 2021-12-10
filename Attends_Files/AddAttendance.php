c69aa41090149be2dfe5a77744459a46cc64958f
Thomas Welch <91126396+Twelch115@users.noreply.github.com>
2021-12-10 09:17:40 +0000

    Update AddAttendance.php

--- Attends_Files/AddAttendance.php
+++ Attends_Files/AddAttendance.php
@@ -2,29 +2,29 @@
 require '../Connection.php'; #the file will not run without Connection.PHP being linked
 require '../Validation.php'; #the file will not run without Validation.PHP being linked
 
-$AttName = Validate($_POST["AttName"]);
-$AttId = $_POST["AttId"];
+$AttName = Validate($_POST["AttName"]); #set data from form as variable
+$AttId = $_POST["AttId"]; #set data from form as variable
 
-$sql = "INSERT INTO attends(mission_name, astronaut_id) VALUES ('$AttName', $AttId)";
+$sql = "INSERT INTO attends(mission_name, astronaut_id) VALUES ('$AttName', $AttId)"; # create query to remove data from table at given id
 
-$sql_get_astro = mysqli_query($connection,"SELECT no_missions FROM astronaut WHERE astronaut_id = $AttId");
-$result = mysqli_fetch_assoc($sql_get_astro);
-$new_no_missions = $result['no_missions'] + 1;
-mysqli_query($connection, "UPDATE astronaut SET no_missions = $new_no_missions WHERE astronaut_id =  $AttId ");
+$sql_get_astro = mysqli_query($connection,"SELECT no_missions FROM astronaut WHERE astronaut_id = $AttId"); #create query to select all data from table relating to ID
+$result = mysqli_fetch_assoc($sql_get_astro); #perform above query and set result as $result
+$new_no_missions = $result['no_missions'] + 1; #set $new_no_missions as result plus one
+mysqli_query($connection, "UPDATE astronaut SET no_missions = $new_no_missions WHERE astronaut_id =  $AttId "); #update mission number in table
 
-$sql_get_miss = mysqli_query($connection, "SELECT crew_size FROM mission WHERE name = '$AttName'");
-$results = mysqli_fetch_assoc($sql_get_miss);
-$new_no_crew = $results['crew_size'] + 1;
-mysqli_query($connection, "UPDATE mission SET crew_size = $new_no_crew WHERE name = '$AttName'");
+$sql_get_miss = mysqli_query($connection, "SELECT crew_size FROM mission WHERE name = '$AttName'"); #create query to select all data from table at given name
+$results = mysqli_fetch_assoc($sql_get_miss); #perform above query and set result as $results
+$new_no_crew = $results['crew_size'] + 1; #set $new_no_crew as results plus one
+mysqli_query($connection, "UPDATE mission SET crew_size = $new_no_crew WHERE name = '$AttName'"); #update crew size in table
 
-if(!mysqli_query($connection, $sql)){
-	die("Error:".mysqli_error($connection));
+if(!mysqli_query($connection, $sql)){ #IF the connection fails
+	die("Error:".mysqli_error($connection)); #end code and print error message
 }
-else{
-	echo "Data Inputed, " ;
+else{ #ELSE
+	echo "Data Inputed, " ; #print success message
 }
 
-mysqli_close($connection);
+mysqli_close($connection); #ends connection with SQL database
 
 echo '<a href = "../index.html">Return </a>' #return to index
 
